83781cbfe3ecb7bb7e07639fa691d8002a8e545a
IT1089778 <nvrnagella@gmail.com>
2023-02-26 10:45:31 +0530

    mongod.conf file

--- mongodb.sh
+++ mongodb.sh
@@ -7,5 +7,5 @@ yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 #update listen address
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod
