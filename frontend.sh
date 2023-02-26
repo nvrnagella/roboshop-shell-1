5df746e81c04170a07578d03b1e4383ffb63afbd
IT1089778 <nvrnagella@gmail.com>
2023-02-26 12:19:02 +0530

    displaying our own comments to the user

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
 source common.sh
 
-#Install Nginx
+echo -e "\e[31m Install Nginx \e[0m"
 yum install nginx -y
-# enable and start nginx
+echo -e "\e[31m enable and start nginx \e[0m"
 systemctl enable nginx
 systemctl start nginx
-#remove old content of the nginx
+echo -e "\e[31m remove old content of the nginx \e[0m"
 rm -rf /usr/share/nginx/html/*
-#download content from remote place
+echo -e "\e[31m download content from remote place \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-#navigagte to that directory
+echo -e "\e[31m navigagte to that directory \e[0m"
 cd /usr/share/nginx/html
-#extract that content
+echo -e "\e[31m extract that content \e[0m"
 unzip /tmp/frontend.zip
-#copy reverse proxy
+echo -e "\e[31m copy reverse proxy \e[0m"
 cp $path_location/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-#restart nginx
+echo -e "\e[31m restart nginx \e[0m"
 systemctl restart nginx
