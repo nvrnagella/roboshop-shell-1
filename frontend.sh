6401daa8b42f014599431fdd601eccdf618c12ad
IT1089778 <nvrnagella@gmail.com>
2023-02-27 20:13:30 +0530

    used function for exit status to keep code dry

--- frontend.sh
+++ frontend.sh
@@ -5,24 +5,24 @@ yum install nginx -y &>> ${LOG}
 status_check
 echo -e "\e[31m enable and start nginx \e[0m"
 systemctl enable nginx &>> ${LOG}
-echo $?
+status_check
 systemctl start nginx
-echo $?
+status_check
 echo -e "\e[31m remove old content of the nginx \e[0m"
 rm -rf /usr/share/nginx/html/*
-echo $?
+status_check
 echo -e "\e[31m download content from remote place \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> ${LOG}
-echo $?
+status_check
 echo -e "\e[31m navigagte to that directory \e[0m"
 cd /usr/share/nginx/html
-echo $?
+status_check
 echo -e "\e[31m extract that content \e[0m"
 unzip /tmp/frontend.zip &>> ${LOG}
-echo $?
+status_check
 echo -e "\e[31m copy reverse proxy \e[0m"
 cp $path_location/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-echo $?
+status_check
 echo -e "\e[31m restart nginx \e[0m"
 systemctl restart nginx
-echo $?
+status_check
