3e0aa71688c86b846c0724de10e6639c1ce70205
IT1089778 <nvrnagella@gmail.com>
2023-02-27 20:27:30 +0530

    we kept code dry by using print_head function to display messages on screen.

--- frontend.sh
+++ frontend.sh
@@ -3,26 +3,26 @@ source common.sh
 print_head "Install Nginx"
 yum install nginx -y &>> ${LOG}
 status_check
-echo -e "\e[31m enable and start nginx \e[0m"
+print_head " enable and start nginx "
 systemctl enable nginx &>> ${LOG}
 status_check
 systemctl start nginx
 status_check
-echo -e "\e[31m remove old content of the nginx \e[0m"
+print_head " remove old content of the nginx "
 rm -rf /usr/share/nginx/html/*
 status_check
-echo -e "\e[31m download content from remote place \e[0m"
+print_head " download content from remote place "
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> ${LOG}
 status_check
-echo -e "\e[31m navigagte to that directory \e[0m"
+print_head " navigagte to that directory "
 cd /usr/share/nginx/html
 status_check
-echo -e "\e[31m extract that content \e[0m"
+print_head " extract that content "
 unzip /tmp/frontend.zip &>> ${LOG}
 status_check
-echo -e "\e[31m copy reverse proxy \e[0m"
+print_head " copy reverse proxy "
 cp $path_location/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 status_check
-echo -e "\e[31m restart nginx \e[0m"
+print_head " restart nginx "
 systemctl restart nginx
 status_check
