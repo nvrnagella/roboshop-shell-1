f985ad6b3e66eccfceba2f7c7c7e92d2b889aebc
IT1089778 <nvrnagella@gmail.com>
2023-03-01 01:59:29 +0530

    updated mogodb

--- redis.sh
+++ redis.sh
@@ -14,4 +14,4 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 status_check
 print_head "Start & Enable Redis Service"
 systemctl enable redis &>> ${LOG}
-systemctl start redis
+systemctl restart redis
